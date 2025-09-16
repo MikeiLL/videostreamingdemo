cadd98ced9977978ce01757d184a663e0f519374
mikeill <mike@mzoo.org>
2025-09-16 11:09:01 -0500

    Make a longer (5 minute) demo video.

--- placholdervideo.sh
+++ placholdervideo.sh
@@ -1,10 +1,10 @@
 ffmpeg -f lavfi -i "
-color=white:1280x720:d=10,
+color=white:1280x720:d=300,
 format=rgb24,
 drawtext=
-  fontcolor=black:
+  fontcolor=red:
   fontsize=600:
   text='%{eif\:mod(600-t, 10)\:d\:2}':
   x=(w-text_w)/2:
   y=(h-text_h)/2
-" 10second.mp4
+" 5minute.mp4
